a1376215556692ffb6abef0a4afe322527ec70c7
1e16miin <ddogi1513@naver.com>
2022-04-06 15:32:19 +0900

    day2 최초 풀이

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,7 +1,14 @@
-(ns aoc2018-2)
+(ns aoc2018-2
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-;; 파트 1
-;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
+
+(defn get-strings-from-input [filename] (-> filename
+                                            io/resource
+                                            slurp
+                                            str/split-lines
+                                            ))
+;; 파트 1;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
 ;; 두번 나타난 문자가 있는 문자열의 수 * 세번 나타난 문자가 있는 문자열의 수를 반환하시오.
 ;; 예)
 ;; abcdef 어떤 문자도 두번 혹은 세번 나타나지 않음 -> (두번 나오는 문자열 수: 0, 세번 나오는 문자열 수: 0)
@@ -13,6 +20,20 @@
 ;; ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3)
 ;; 답 : 4 * 3 = 12
 
+(defn part1-solution [strings] (loop [strings strings
+                                      dup-twice-count 0
+                                      dup-thrice-count 0]
+                                 (let [cur-string (first strings)]
+                                   (let [freq-cur-string (vals (frequencies cur-string))]
+                                     (if (some #(= 3 %) freq-cur-string)
+                                       (let [dup-thrice-count (+ dup-thrice-count 1)]))
+                                     (if (some #(= 2 %) freq-cur-string)
+                                       (let [dup-twice-count (+ dup-twice-count 1)]))
+                                     (recur (next strings) dup-twice-count dup-thrice-count))
+                                   )
+                                ;;  (* dup-twice-count dup-thrice-count)
+                                 ))
+(comment (part1-solution (get-strings-from-input "day2.sample.txt")))
 
 ;; 파트 2
 ;; 여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.

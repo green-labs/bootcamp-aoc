7c0b378e964b995c099e31545417f7ab7bec0519
1e16miin <ddogi1513@naver.com>
2022-04-07 14:51:30 +0900

    day2 part1 solution

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,29 +1,15 @@
 (ns aoc2018-2
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
 
 
-;; (defn get-strings-from-input 
-;;   [filename] 
-;;   (-> filename
-;;       io/resource
-;;       slurp
-;;       str/split-lines))
-
-;;       (abcdef bababac)
-;;       -> get-freq ([1] [1 3])
-;;       -> 
-;; 파트 1;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
-;; 두번 나타난 문자가 있는 문자열의 수 * 세번 나타난 문자가 있는 문자열의 수를 반환하시오.
-;; 예)
-;; abcdef 어떤 문자도 두번 혹은 세번 나타나지 않음 -> (두번 나오는 문자열 수: 0, 세번 나오는 문자열 수: 0)
-;; bababc 2개의 a, 3개의 b -> (두번 나오는 문자열 수: 1, 세번 나오는 문자열 수: 1)
-;; abbcde 2개의 b -> (두번 나오는 문자열 수: 2, 세번 나오는 문자열 수: 1)
-;; abcccd 3개의 c -> (두번 나오는 문자열 수: 2, 세번 나오는 문자열 수: 2)
-;; aabcdd 2개의 a, 2개의 d 이지만, 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 3, 세번 나오는 문자열 수: 2)
-;; abcdee 2개의 e -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 2)
-;; ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3)
-;; 답 : 4 * 3 = 12
+(defn get-strings-from-input
+  [filename]
+  (-> filename
+      io/resource
+      slurp
+      str/split-lines))
 
 ;; 이걸 왜 쓸까요? 가독성을 높이기 위해서 -> 함수를 중첩된 가장 깊숙한 곳 부터 읽기 vs 위에서 아래로 읽기
 ;; https://rescript-lang.org/docs/manual/latest/pipe
@@ -59,15 +45,30 @@
                 vals
                 set))
 
-'("abcdef" "bababc")
-(->> '("abcdef" "bababc")
-     (map get-freq-set))
+
+
+
 ;; loop recur로 풀어보기
-(defn part1-solution [strings]
-  (loop [strings strings]
+
+(defn is-contain
+  "set에 number가 있으면 input으로 받는 count를 1 증가 시켜주는 함수
+   input: 2 {2 3} 1
+   output: 2"
+  [number freq-set cnt]
+  (if (freq-set number)
+    (+ cnt 1)
+    cnt))
+
+;;thread macro로 refactoring 하기
+(defn part1-solution-loop-recur [strings]
+  (loop [strings strings
+         dup-twice-cnt 0
+         dup-thirce-cnt 0]
     (let [cur-string (first strings)
-          freq-])))
-                                 ;; format on save
+          freq-set (get-freq-set cur-string)]
+      (if (== (count strings) 0)
+        (* dup-thirce-cnt dup-twice-cnt)
+        (recur (next strings) (is-contain 2 freq-set dup-twice-cnt) (is-contain 3 freq-set dup-thirce-cnt))))))
 
 ;; loop-recur & 중간중간 데이터 변환하는 함수 구현하기
 ;; 
@@ -78,20 +79,30 @@
 ;;     탈출해라!
 ;;     재귀호출해라!)
 
-(comment (part1-solution (get-strings-from-input "day2.sample.txt")))
 
-;; 파트 2
-;; 여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.
-;; 예)
-;; abcde
-;; fghij
-;; klmno
-;; pqrst
-;; fguij
-;; axcye
-;; wvxyz
 
-;; 주어진 예시에서 fguij와 fghij는 같은 위치 (2번째 인덱스)에 정확히 한 문자 (u와 h)가 다름. 따라서 같은 부분인 fgij를 리턴하면 됨.
+
+;;step1 모든 순서쌍 구하기
+;;step2 같은 위치에 있지 않은 것만 filter
+;;step3 filter된 리스트의 길이가 1인지 확인
+;;step4 길이가 1인 나머지 부분만 return
+
+(defn part2-solution1
+  [strings]
+  (->> strings
+       comb/combinations
+       count))
+
+(comment
+  (->> "day2.sample.txt"
+       get-strings-from-input
+       part1-solution-loop-recur)
+  ;; (->> "day2.sample.txt"
+  ;;      get-strings-from-input
+  ;;      part2-solution1)
+  )
+
+
 
 
 ;; #################################

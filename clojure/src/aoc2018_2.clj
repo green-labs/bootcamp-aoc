9fdcb997bd48c586493add448a4f7887aa65b1c7
arthur <>
2022-05-11 16:37:08 +0900

    일단은 그럴듯한 계획

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,4 +1,7 @@
-(ns aoc2018-2)
+(ns aoc2018-2
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
 
 ;; 파트 1
 ;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
@@ -12,6 +15,33 @@
 ;; abcdee 2개의 e -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 2)
 ;; ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3)
 ;; 답 : 4 * 3 = 12
+
+;;일단 소스
+(def day2-source
+  (->> (io/resource "day2.sample.txt")
+       slurp ;;메모리라..더 좋은게 필요한데..
+       (str/split-lines)))
+
+(println day2-source)
+
+;; map을 써보자 {}
+
+(defn getChars [text]
+  (println text)
+  (println "###########"))
+
+(defn getlines [value]
+  (for [text value] (getChars [(seq text)])))
+
+(defn day2-1 []
+  (getlines[day2-source]))
+
+
+(day2-1)
+
+
+((map #(str %) day2-source)
+ -> ())
 
 
 ;; 파트 2

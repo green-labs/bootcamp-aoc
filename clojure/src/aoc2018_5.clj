978c6feaf71a8f78007ab05d2be83bca792e5c9e
조이현 <ihyuncho@Ihyun-makbook.local>
2022-09-05 15:45:20 +0900

    aoc2018 day5: solve both

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,4 +1,6 @@
-(ns aoc2018_5)
+(ns aoc2018_5
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]))
 ;; 파트 1
 ;; 입력: dabAcCaCBAcCcaDA
 
@@ -10,7 +12,87 @@
 
 ;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
 
+
+(defn parse-file-to-str
+  "parse file input to string
+   input: string file
+   output: string"
+  [input]
+  (->  input
+       io/resource
+       slurp))
+
+(defn convert-char-case
+  "convert character to opposite case of letter
+   input: a
+   output: A"
+  [in-char]
+  (if (Character/isUpperCase in-char)
+    (first (string/lower-case in-char))
+    (first (string/upper-case in-char))))
+
+(defn react-polymer
+  "execute to react polymer fully
+   input: aAbdbcCBd
+   output: bdd
+   "
+  [out in]
+  (if
+    (empty? in)
+     out 
+     (recur (if (or
+                 (empty? out)
+                 (not= (convert-char-case (peek out)) (first in)))
+        (conj out (first in)) ;;push char to stack if there is no reaction
+        (pop out)) ;;pop the last char from stack if there is reaction
+      (rest in))))
+
+(comment
+  (->> "2018_5_sample.txt"
+       ;parse
+       parse-file-to-str
+
+       ;preprocess
+       (react-polymer [])
+       
+       ;aggregate
+       (apply str)
+       
+       ;print
+       count)
+  )
+
 ;; 파트 2
 ;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
 ;; 예를 들어 dabAcCaCBAcCcaDA 에서 a/A를 없애고 모두 반응시키면 dbCBcD가 되고 길이는 6인데 비해,
 ;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
+
+
+(defn make-map-removing-unit
+  "make a map after removing unit from aA to zZ
+   input: dabAcCa
+   output: ((d b c C) (d a A c C a) (d a b A a)...)
+   "
+  [input]
+  (for [unit
+        (map char (range (int \a) (int \z)))]
+    (filter #(and
+              (not= unit %)
+              (not= (convert-char-case unit) %))
+            input))
+  )
+
+(comment
+  (->> "2018_5_sample.txt"
+
+       ;parse
+       parse-file-to-str
+
+       ;preprocess
+       make-map-removing-unit
+       (map #(apply str %))
+       (map #(count (react-polymer [] %)))
+
+       ;aggregate & print
+       (apply min))
+)

b93a49f75b079c2d0da016abce805a997c40f2aa
조이현 <ihyuncho@Ihyun-makbook.local>
2022-09-13 09:32:24 +0900

    aoc2018_7 solve part1

---
+++ clojure/src/aoc2018_7.clj
@@ -0,0 +1,176 @@
+(ns aoc2018_7
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]))
+
+(def step-regex #"Step (\S) must be finished before step (\S) can begin.")
+
+(defn parse-file-to-str
+  "parse file input to string
+   input: string file
+   output: string"
+  [input]
+  (->  input
+       io/resource
+       slurp
+       string/split-lines))
+
+(defn make-step-table
+  "make a table map indicating step requrirement
+   input: \"Step C must be finished before step A can begin.\"
+   output: {:req C, :step A}
+   "
+  [input]
+  (let [[_ req step] (re-find step-regex input)] 
+    {:req (first req)
+     :step (first step)}))
+
+(defn make-dummy-table
+  "add dummy table for steps which don't have requirement
+   input: 
+   {:req C, :step A}
+   {:req C, :step F}..
+   output:
+   {:req D, :step E}
+   {:req F, :step E}
+   {:req d, :step A}
+   {:req d, :step B}
+   "
+  [input]
+  (let [standard-time (dec (int \A))]
+    (concat input
+            (for [ch (range (int \A) (inc (int \F)))]
+              {:req \d :step (char ch) :time (- ch standard-time) :sched false}))))
+
+(defn find-available-step
+  "find available step which 1) doesn't have dependency to other step 2) first in alphabetical order
+   input: 
+   {:req D, :step E}
+   {:req F, :step E}
+   {:req d, :step A}
+   {:req d, :step B}..
+   output: C "
+  [input]
+  (->> input
+       (map :step)
+       frequencies 
+       (sort-by first) ;sort by alphabet
+       (sort-by second) ;sort by frequency
+       first ;minimum frequency
+       first ;key
+       )
+)
+
+(defn schedule-work
+  "gather available steps in each timing after repeating removing dependency and find available step
+   input: 
+   {:req D, :step E}
+   {:req F, :step E}
+   {:req d, :step A}
+   {:req d, :step B}...
+   output: \"CABDFE\""
+  [input]
+(loop [in input
+       res ""]
+  (if (empty? in)
+    res
+    (let [available-step (find-available-step in)]
+     (recur (remove #(or (= (:step %) available-step) (= (:req %) available-step)) in)
+      (str res available-step))) 
+  )
+  ))
+
+(comment
+  (->> "2018_7_sample copy.txt"
+       
+       ;parse
+       parse-file-to-str
+       (map make-step-table)
+       
+       ;process
+       make-dummy-table
+
+       ;aggregate
+      ; schedule-work)
+       
+))
+
+
+(dec (int \b))
+
+
+(def ds '({:req \C, :step \A}
+         {:req \C, :step \F}
+         {:req \A, :step \B}
+         {:req \A, :step \D}
+         {:req \B, :step \E}
+         {:req \D, :step \E}
+         {:req \F, :step \E}
+         {:req \d, :step \A, :time 1}
+         {:req \d, :step \B, :time 2}
+         {:req \d, :step \C, :time 3}
+         {:req \d, :step \D, :time 4}
+         {:req \d, :step \E, :time 5}
+         {:req \d, :step \F, :time 6}))
+
+(map #(if (= (:req %) \d) (update % :time dec) %)ds)
+
+
+(defn find-available-steps-in-5
+  "find available five steps which 1) doesn't have dependency to other step 2) first in alphabetical order
+   input: 
+   {:req D, :step E}
+   {:req F, :step E}
+   {:req d, :step A}
+   {:req d, :step B}..
+   output: C, D.. "
+  [input]
+  (->> input
+       (map :step)
+       frequencies
+       (sort-by first) ;sort by alphabet
+       (sort-by second) ;sort by frequency
+       (filter #(= (second %) 1)) ;appeared only once
+       (take 5) ;limit
+       (map first)
+       ))
+
+
+#_(defn schedule-available-work
+  [input]
+  (let []
+   
+  ))
+(defn schedule-work-in-seconds
+  "calculate seconds to execute steps in each timing after repeating removing dependency and find available step
+   input: 
+   {:req D, :step E}
+   {:req F, :step E}
+   {:req d, :step A}
+   {:req d, :step B}...
+   output: 15 "
+  [input]
+  (loop [in input
+         seconds 0]
+    (if (empty? in)
+      seconds
+      (let [available-step (find-available-steps-in-5 in)]
+        (recur (remove #(and (= (:step %) available-step) (= (:req %) available-step)) in)
+               (inc seconds))))))
+
+(take 5 '(1 2 3))
+
+(comment
+  (->> "2018_7_sample copy.txt"
+       
+       ;parse
+       parse-file-to-str
+       (map make-step-table)
+
+       ;process
+       make-dummy-table
+
+       ;aggregate
+       find-available-steps-in-5
+       ;schedule-work-in-seconds
+
+       ))

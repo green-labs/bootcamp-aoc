9eaecf5a8182c18c6115139d6fcf383927c26d48
Yummy-sk <kunshup2000@gmail.com>
2022-11-17 11:33:26 +0900

    feat: day5 셋팅 및 part1 JS 풀이

---
+++ rescript/src/Week1/Year2020Day5.bs.js
@@ -0,0 +1,57 @@
+/**
+ * 1. 데이터를 입력 받는다. ( getInputData )
+ * 2. solution 함수로 데이터를 넘긴다. ( solution )
+ * 3. solution 함수에서는 데이터를 받아서, map을 통해 각 데이터를 처리한다. ( solution )
+ * 4. 각 데이터를 2진수로 변환한다. ( getBinaryNumber )
+ * 5. 2진수를 10진수로 변환한다. ( getDecimalNumber )
+ * 6. 10진수를 반환한다. ( getDecimalNumber )
+ * 7. 반환된 10진수를 배열로 만들어서 반환한다. ( getDecimalNumber )
+ * 8. 반환된 데이터의 Max 값을 찾는다. ( getMaximumNumber )
+ * 9. Max 값을 반환한다. ( getMaximumNumber )
+ * 10. 반환된 Max 값을 출력한다.
+ */
+
+const getInputData = () =>
+  require('fs')
+    .readFileSync('../../input/Week1/Year2020Day5.sample.txt', 'utf8')
+    .toString()
+    .trim()
+    .split('\n');
+
+const getBinaryNumber = input => {
+  const dataSet = {
+    F: '0',
+    B: '1',
+    L: '0',
+    R: '1',
+  };
+
+  return input
+    .split('')
+    .map(data => dataSet[data])
+    .join('');
+};
+
+const getDecimalNumber = binary => parseInt(binary, 2);
+
+const getMaximumNumber = numbers => Math.max(...numbers);
+
+const calculateSeatId = inputs => {
+  return inputs.map(input => {
+    const binary = getBinaryNumber(input);
+    const decimal = getDecimalNumber(binary);
+
+    return decimal;
+  });
+};
+
+const solution = inputs => {
+  const seatIds = calculateSeatId(inputs);
+
+  return getMaximumNumber(seatIds);
+};
+
+const input = getInputData();
+const result = solution(input);
+
+console.log(result);
